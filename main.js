6fc6227d2d7189e989aef48889a147b6a164858e
schuchard <kbs177@gmail.com>
2015-02-18 21:12:51 -0700

    moved extra test to beginning for loop.

--- main.js
+++ main.js
@@ -1,35 +1,42 @@
 
-var people = 10;
-var groups = 3 ;
+var people = 23;
+var groups = 9  ;
 
+// Finds how many people fit into each group
 var minPeople = people / groups;
-console.log('minPeople: ', minPeople);
+// console.log('minPeople: ', minPeople);
 
-var subtractor = Math.floor(minPeople);
-console.log('subtractor: ', subtractor);
+// Minimum whole people in each group
+var minPeopleInt = Math.floor(minPeople);
+// console.log('minPeopleInt: ', minPeopleInt);
 
-var remainder = (minPeople - subtractor);
-console.log('remainder: ',remainder);
+// Pulls remaining percentage of people left over
+var remainder = (minPeople - minPeopleInt);
+// console.log('remainder: ',remainder);
 
-var chunks = Math.round(remainder * groups);
-console.log('chunks: ', chunks);
-var extra = chunks;
+// Remaining people to be added to the
+// first groups evenly
+var extras = Math.round(remainder * groups);
+// console.log('extras: ', extras);
 
 var printer = [];
 var y = 1;
 
+// Loop through the number of groups, adding extras
+// to the beginning groups
 for (var i = 0; i < groups; i++) {
         var currentArray = [];
-    for (var x = 0; x < subtractor; x++) {
-        currentArray.push(y);
-            if(extra > 0){
-              y += 1;
-              extra -= 1;
+            // Check for extras people, add one if so
+            if(extras > 0){
               currentArray.push(y);
+              y += 1;
+              extras -= 1;
             }
+    // Add minimum people to each group
+    for (var x = 0; x < minPeopleInt; x++) {
+        currentArray.push(y);
         y += 1;
     }
-
     printer.push(currentArray);
 }
 
